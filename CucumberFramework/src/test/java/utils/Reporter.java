079b64727ee5c245db8e4529de7a14493ad8bdd0
Babu <homework@testleaf.com>
2017-10-15 20:38:55 +0530

    Updated the extent config path

--- CucumberFramework/src/test/java/utils/Reporter.java
+++ CucumberFramework/src/test/java/utils/Reporter.java
@@ -45,7 +45,7 @@ public abstract class Reporter {
 	public ExtentReports startResult(){
 		testDriver = new HashMap<RemoteWebDriver, ExtentTest>();
 		extent = new ExtentReports("./reports/result.html", false);
-		extent.loadConfig(new File("./src/main/resources/extent-config.xml"));
+		extent.loadConfig(new File("./extent.xml"));
 		return extent;
 	}
 
